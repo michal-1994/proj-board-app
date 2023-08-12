6f740d22447ca427815e0b63669742fd6f4c1ce0
Michal <michalgrzegorczyk04@gmail.com>
2023-08-12 20:06:21 +0200

    feat: moving items between lists

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 $(function () {
-    $("#sortable-row").sortable();
-    $("#sortable-list").sortable();
+    let oldList, newList, item;
+
+    $(".sortable-row").sortable();
+    $(".sortable-list")
+        .sortable({
+            start: function (event, ui) {
+                item = ui.item;
+                newList = oldList = ui.item.parent().parent();
+            },
+            stop: function (event, ui) {},
+            change: function (event, ui) {
+                if (ui.sender) newList = ui.placeholder.parent().parent();
+            },
+            connectWith: ".sortable-list",
+        })
+        .disableSelection();
 });
